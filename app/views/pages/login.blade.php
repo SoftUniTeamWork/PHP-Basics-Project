e9364ba1c321fa4b66fb20bc9811b00f524e4a6b
Georgi Stoyanov <gstoyanov@live.com>
2014-12-14 14:10:02 +0200

    added registration form

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -19,4 +19,28 @@
 			<input type="submit" value="Submit">
 		</p>
 	</form>
+	
+	<form action="post">
+	    <h1>Registration form</h1>
+	    
+	    <p>
+	        <label for="userName">User Name</label>
+	        <input type="text" name="userName"/>
+	    </p>
+	    <p>
+	        <label for="email">Email</label>
+	        <input type="text" name="email"/>
+	    </p>
+	    <p>
+	        <label for="passowrd">Password</label>
+	        <input type="password" name="password"/>
+	    </p>
+	    <p>
+        	 <label for="passowrd">Repeat Password</label>
+             <input type="password" name="passwordRepeat"/>
+        </p>
+        <p>
+        	 <input type="submit" value="Submit">
+        </p>
+	</form>
 @stop
