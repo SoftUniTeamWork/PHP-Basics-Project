f6181ad0ed86b3ae787ab6cafdb689633563804d
Georgi Stoyanov <gstoyanov@live.com>
2014-12-15 19:17:32 +0200

    import ckEditor

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -2,5 +2,6 @@
 <title>Document</title>
 <script type="text/javascript" src="{{ URL::asset('js/jquery-2.1.1.min.js') }}"></script>
 <script src="js/kickstart.js"></script>
+<script src="../js/ckeditor/ckeditor.js"></script>
 <link rel="stylesheet" href="{{ URL::asset('css/kickstart.css') }}" media="all" />
 <link rel="stylesheet" href="{{ URL::asset('css/blogstyle/style.css') }}" media="all" />
--- app/views/pages/createPost.blade.php
+++ app/views/pages/createPost.blade.php
@@ -9,7 +9,14 @@
 			</p>
 			<p>
 				<label for="text">Text</label>
-				<textarea name="text"></textarea>
+				<textarea name="text" id="editor1" rows="10" cols="80">
+                                This is my textarea to be replaced with CKEditor.
+                            </textarea>
+                            <script>
+                                // Replace the <textarea id="editor1"> with a CKEditor
+                                // instance, using default configuration.
+                                CKEDITOR.replace( 'editor1' );
+                            </script>
 			</p>
 			<p>
 				<label for="tags">Tags</label>
