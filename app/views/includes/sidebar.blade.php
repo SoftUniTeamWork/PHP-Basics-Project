d97a1e011e263b1a4eeae249a456d2fc376a3fb7
Gleomit <draychev95@mail.ru>
2014-12-15 20:44:52 +0200

    fixed somethings

--- app/views/includes/sidebar.blade.php
+++ app/views/includes/sidebar.blade.php
@@ -2,13 +2,13 @@
     <h3>Recent Posts</h3>
     <ul>
     @foreach($recentPosts as $post)
-       <li><a href="">{{$post->id}}</a></li>
+       <li><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
     @endforeach
     </ul>
     <h3>Most Visited Posts</h3>
     <ul>
     @foreach($mostVisited as $post)
-       <li><a href="">{{$post->id}}</a></li>
+       <li><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
     @endforeach
     </ul>
 </aside>
