313bcbe066ec68b6daf035bc7b83876076239d00
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-16 13:28:21 +0200

    Adding paggination

--- app/views/includes/sidebar.blade.php
+++ app/views/includes/sidebar.blade.php
@@ -1,16 +1,18 @@
     <div class="panel panel-default">
-    	<div class="panel-heading">
+    	<div class="panel-heading text-left">
 	    	<h3>Recent Posts</h3>
     	</div>
-	    <ul class="list-group">
+	    <ol type = "1" class="list-group">
 	    @foreach(Post::orderBy('created_at','desc')->paginate(10) as $post)
-	       <li class="list-group-item"><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
+	       <li class="list-group-item text-left"><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
 	    @endforeach
-	    </ul>
-	    <h3>Most Visited Posts</h3>
-	    <ul class="list-group">
+	    </ol>
+	    <div class="panel-heading text-left">
+            <h3>Most Visited Posts</h3>
+        </div>
+	    <ol type = "1" class="list-group">
 	    @foreach(Post::orderBy('visits_counter','desc')->paginate(10) as $post)
-	       <li class="list-group-item"><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
+	       <li class="list-group-item text-left"><a href="{{ url('/post/' . $post->id) }}">{{$post->post_title}}</a></li>
 	    @endforeach
-	    </ul>
+	    </ol>
     </div>
