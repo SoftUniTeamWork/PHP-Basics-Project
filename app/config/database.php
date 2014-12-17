58a70a8a2c67ba9750698b5781ed62af74681162
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-17 23:48:40 +0200

    commit after crash ;(

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'greenarm_blog_database',
-			'username'  => 'greenarm_root',
-			'password'  => '123123Gs',
+			'database'  => 'blog_database',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
