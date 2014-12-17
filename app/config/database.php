bf906b33e4aa4a6c6bf813ad5bc1f5030ed1b9fb
gstoyanov90 <gstoyanov@live.com>
2014-12-17 23:18:54 +0200

    changed database user/pass & phpversion

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'blog_database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'greenarm_blog_database',
+			'username'  => 'greenarm_root',
+			'password'  => '123123Gs',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
