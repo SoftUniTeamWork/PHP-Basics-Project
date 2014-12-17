be788ba685c5d59017f5adf48dfa6004b2487478
gstoyanov90 <gstoyanov@live.com>
2014-12-17 23:34:57 +0200

    background fixed to bottom of page

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<section class="container">
+<section class="container footerBottom">
 	<p class="text-center">
 		<a href="{{ url('/') }}">&copy; Team London</a> | <a href="https://softuni.bg">SoftUni</a>
 	</p>
