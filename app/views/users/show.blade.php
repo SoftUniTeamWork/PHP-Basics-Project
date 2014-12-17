eec365cfcdbeff2691cf68eb2e98cef2a0514692
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-17 19:41:03 +0200

    Styled user profile page

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,12 +2,46 @@
 
 @section('content')
 	@if(isset($user))
-	<section>
-		<h1>Username - {{$user->username}}</h1>
-		<p>Name - @if($user->name) {{$user->name}} @else {{'not set'}} @endif</p>
-		<p>Age - @if($user->age) {{$user->age}} @else {{'not set'}} @endif</p>
-	</section>
+	<section class="userPanel">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 " >
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h1 class="panel-title">{{$user->username}}</h1>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-6 col-lg-6 " align="center">
+                    <img alt="User Pic" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=100" class="img-circle">
+                </div>
+                <div class=" col-md-9 col-lg-9 ">
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td>Name:</td>
+                        <td>@if($user->name) {{$user->name}} @else {{'not set'}} @endif</td>
+                      </tr>
+                      <tr>
+                        <td>Age:</td>
+                        <td>@if($user->age) {{$user->age}} @else {{'not set'}} @endif</td>
+                      </tr>
+                      <tr>
+                        <td>Email:</td>
+                        <td>@if($user->email) {{$user->email}} @else {{'not set'}} @endif</td>
+                      </tr>
+                      <tr>
+                        <td>Total comments:</td>
+                        <td>99999</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <a href="#" class="btn btn-primary">View all commented posts</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
 	@else
 		<h1>This user does not exist!</h1>
 	@endif
-@stop
+@stop 
