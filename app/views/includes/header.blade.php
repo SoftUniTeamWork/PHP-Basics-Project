9783aef957305eca61797f74f2e7dfa1206787e7
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-17 13:29:17 +0200

    Erased Contacts from header

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -2,7 +2,6 @@
 	<ul class="nav navbar-nav">
 		<li><a href="{{url('/')}}">Home</a></li>
 		<li><a href="{{url('/about')}}">About</a></li>
-		<li><a href="{{url('/contacts')}}">Contacts</a></li>
 		<li><a href="{{url('/credits')}}">Credits</a></li>
 	</ul>
 	@if(Auth::check())
