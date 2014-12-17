668c619bc4131603306a6f20d6203e404229cf8f
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-17 23:16:57 +0200

    minor style change

--- app/views/includes/sidebar.blade.php
+++ app/views/includes/sidebar.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="search">
         <input type="text" class="form-control input-sm" maxlength="64" placeholder="Search" />
-         <button type="submit" class="btn btn-primary btn-sm">Search</button>
+         <button type="submit" id="button" class="btn btn-primary btn-sm">Search</button>
         </div>
     </div>
 
