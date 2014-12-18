71c5657d9eaaeb55546efb2be521f3332e643f8e
SStoyanov22 <SStoyanov22@gmail.com>
2014-12-18 14:59:02 +0200

    Added About and Credits content

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -6,14 +6,14 @@
         <article class="row">
             <div class="col-lg-8">
                 <h2>Who are we?</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>We are a team of students from Software University. If you want to see the code behind the project you can find it <a href="https://github.com/SoftUniTeamWork/PHP-Basics-Project"><span class="badge">here</span></a>. If you want to know more about us and what everyone contributed to the project with go to the About page.</p>
             </div>
         </article>
 
         <article class="row">
             <div class="col-lg-8">
                 <h2>What is the project about</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>The project is a part of our Teamwork Project Assignment for the PHP Basics Course at Software University. We have chosen to make a blog for the functionality a blog requires. We met a lot of obstacles on way but the Laravel framework helped us a lot for maintaining order and giving it all the functionality it has now.</p>
             </div>
         </article>
     </section>
--- app/views/pages/credits.blade.php
+++ app/views/pages/credits.blade.php
@@ -7,8 +7,8 @@
             <div class="col-lg-4"><img class="img-responsive" src="{{ URL::asset('images/images.png') }}" alt=""/></div>
             <div class="col-lg-8">
                 <h2>Deivid Raychev</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+                <p>Deivid is the team leader. He came up with the idea to use Laravel because it would ease our work. He made most of the functionality of the site, made some styilisation and all in all was the mind behind the project.</p>
+                </div>
         </article>
 
         <article class="row">
@@ -28,10 +28,10 @@
         </article>
 
         <article class="row">
-            <div class="col-lg-4"><img class="img-responsive" src="{{ URL::asset('images/images.png') }}" alt=""/></div>
+            <div class="col-lg-4"><img class="img-responsive" src="{{ URL::asset('images/emo.jpg') }}" altemo.jpg=""/></div>
             <div class="col-lg-8">
                 <h2>Emil Petrov</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>He didn't contribute with anything significant to the project. </p>
             </div>
         </article>
     </section>
